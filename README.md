5782a88533dcf1e20036f3e21c5a8eda2e51bd63
husnilk <i_am_nuniel@yahoo.com.sg>
2018-04-01 21:53:37 +0700

    00 : tugas review php, percabangan

---
+++ README.md
@@ -0,0 +1,14 @@
+# PWEB M09b : Intro PHP
+
+## Instruksi
+
+Lengkapi program di repository ini. Program ini bertujuan untuk menampilkan seluruh faktor dari sebuah bilangan. Faktor adalah bilangan yang membagi bilangan lain tanpa sisa.
+
+## Tujuan
+
+Tugas ini merupakan review dari materi pemrograman PHP, yaitu `perulangan`.
+
+## Info Lain
+
+*   Deadline : Senin/9 April 2018
+*   Sifat Tugas : Perorangan
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tugas M09b</title>
+</head>
+<body>
+    <h2>Mesin Pencari Faktor Bilangan</h2>
+
+    <!-- TODO 1: Tambahkan atribut form ini sehingga data dikirim ke file ini dengan method POST -->
+
+    <form>
+        Bilangan
+        <!-- TODO 2: Tambahkan input berupa text bernama `bilangan` -->
+
+        <button type="submit">Kirim</button>
+    </form>
+
+    <?php
+        //TODO 3 : Lakukang pencarian faktor bilangan disini dan tampilkan 
+
+    ?>
+</body>
+</html>
